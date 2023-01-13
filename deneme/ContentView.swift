ad3b1ae9e84f71fb13542baf1455dbd9b4703cc3
Irmak Atılgan <irmak.atilgan@hisarschool.k12.tr>
2023-01-13 13:27:44 +0300

    Initial Commit

---
+++ deneme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  deneme
+//
+//  Created by Irmak Atılgan on 13.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ deneme/denemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  denemeApp.swift
+//  deneme
+//
+//  Created by Irmak Atılgan on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct denemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
